f79f16abf816e3acff23dbfd0718419cefc2f11d
tatecarson <tatecarson@gmail.com>
2018-06-29 13:27:04 -0500

    another change to bash script

--- update.sh
+++ update.sh
@@ -2,3 +2,6 @@
 
 cd /Users/tatecarson/Documents/webDev/Websites/Pathways/ddem/programming-digital-media; 
 git submodule update --remote themes/hugo-pathways-theme; 
+git commit -m "updated theme"; 
+git pull origin master; 
+git push origin master; 
