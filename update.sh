4918cf8137d8e66180bfe9da1060c19c26ab62be
tatecarson <tatecarson@gmail.com>
2018-06-30 11:55:42 -0500

    updated custom footer

--- update.sh
+++ update.sh
@@ -1,7 +1,47 @@
 #!/bin/bash
 
-cd /Users/tatecarson/Documents/webDev/Websites/Pathways/ddem/programming-digital-media; 
-git submodule update --remote themes/hugo-pathways-theme; 
-git commit -m "updated theme"; 
-git pull origin master; 
-git push origin master; 
+# pulls new material from cms and theme
+# redeploys 
+function update {
+  git pull origin master; 
+  git submodule update --remote themes/hugo-pathways-theme; 
+  git add .; 
+  git commit -m "updated theme"; 
+  git push origin master; 
+}
+
+# engineering
+engineering=/Users/tatecarson/Documents/webDev/Websites/Pathways/pre-engineering
+for dir in "$engineering"/*; do
+    if test -d "$dir"; then
+      cd $dir; 
+      update; 
+    fi
+done
+
+# ddem
+ddem=/Users/tatecarson/Documents/webDev/Websites/Pathways/ddem
+for dir in "$ddem"/*; do
+    if test -d "$dir"; then
+      cd $dir; 
+      update; 
+    fi
+done
+
+# computational thinking 
+cs=/Users/tatecarson/Documents/webDev/Websites/Pathways/computational-thinking
+for dir in "$cs"/*; do
+    if test -d "$dir"; then
+      cd $dir; 
+      update; 
+    fi
+done
+
+# computational thinking 
+bio=/Users/tatecarson/Documents/webDev/Websites/Pathways/biomedical
+for dir in "$bio"/*; do
+    if test -d "$dir"; then
+      cd $dir; 
+      update; 
+    fi
+done
